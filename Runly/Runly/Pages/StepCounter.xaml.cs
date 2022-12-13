039094fd7f40b1b88f28f8eac141b72108c4b997
barttob <barttob@gmail.com>
2022-12-13 15:58:27 +0100

    Krokomierz dzialanie w tle

--- Runly/Runly/Pages/StepCounter.xaml.cs
+++ Runly/Runly/Pages/StepCounter.xaml.cs
@@ -1,5 +1,7 @@
-﻿using Xamarin.Forms;
+﻿using Xamarin.Essentials;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
 
 namespace Runly.Pages
 {
@@ -20,6 +22,19 @@ namespace Runly.Pages
                     amountSteps.Text = message;
                 });
             });
+
+            if (Preferences.Get("LocationServiceRunning", false) == true)
+            {
+                StartService();
+            }
+        }
+
+        private void StartService()
+        {
+            var startServiceMessage = new StartServiceMessage();
+            MessagingCenter.Send(startServiceMessage, "ServiceStarted");
+            Preferences.Set("StepCounterService", true);
+            //amountSteps.Text = "Location Service has been started!";
         }
     }
 }
